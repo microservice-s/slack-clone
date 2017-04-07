e3fd2d32a3e3359706ec6199f4c4f6354ce17da1
aethan <aethan@uw.edu>
2017-04-06 18:18:08 -0700

    Start client

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,12 +2,10 @@
 
 # docker on 16.04 id: 23755729
 # POST request to set up a new droplet
-create_api_server()
+create_server()
 {
-    curl -X POST 
-    -H "Content-Type: application/json" 
-    -H "Authorization: Bearer $TOKEN" 
-    -d '{
+    body=$(cat  << EOF
+    {
         "name":"$1",
         "region":"sfo2",
         "size":"512mb",
@@ -19,40 +17,23 @@ create_api_server()
         "private_networking":null,
         "volumes": null,
         "tags":["api"]
-        }' "https://api.digitalocean.com/v2/droplets"
+    }
+EOF
+)
+    curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d "$body"  "https://api.digitalocean.com/v2/droplets"
 }
 
-create_web_server()
-{
-    curl `-X POST 
-    -H "Content-Type: application/json" 
-    -H "Authorization: Bearer $TOKEN" 
-    -d '{
-        "name":"$1",
-        "region":"sfo2",
-        "size":"512mb",
-        "image":"nginx",
-        "ssh_keys":[7926321],
-        "backups":false,
-        "ipv6":true,
-        "user_data":null,
-        "private_networking":null,
-        "volumes": null,
-        "tags":["web"]
-        }' "https://api.digitalocean.com/v2/droplets"`
-}
 
-echo "Enter the type of server you would like to create (api/web)"
-read typ
 echo "Enter the name of your server"
 read name
-#echo "How many servers would you like to create?"
-#read num
 
-if [ $typ == "api" ]; then
-    create_api_server $name
-else if [ $typ == "web" ]; then
-    create_web_server $name
-    fi
-fi
+echo "How many servers would you like to create?"
+read num
+
+# if [ $typ == "api" ]; then
+#     create_api_server $name
+# else if [ $typ == "web" ]; then
+#     create_web_server $name
+#     fi
+# fi
 
---
+++ webclient/www/script.js
@@ -0,0 +1,9 @@
+function loadOGP() {
+    var title = document.getElementById("title")
+    var description = document.getElementById("description")
+    var image = document.getElementById("image")
+
+    fetch('http://localhost:4000/v1/summary?url=http://ogp.me/')
+}
+
+loadOGP()
