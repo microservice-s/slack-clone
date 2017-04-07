7de4f78f9ab5c97dc0608506e6f2545c98021af9
aethan <aethan@uw.edu>
2017-04-07 08:35:43 -0700

    Add script

--- webclient/www/script.js
+++ webclient/www/script.js
@@ -3,7 +3,11 @@ function loadOGP() {
     var description = document.getElementById("description")
     var image = document.getElementById("image")
 
-    fetch('http://localhost:4000/v1/summary?url=http://ogp.me/')
+    fetch('http://localhost:4000/v1/summary?url=http://ogp.me/', {
+        method: 'get'
+    }).then(function(resp) {
+        
+    })
 }
 
 loadOGP()
