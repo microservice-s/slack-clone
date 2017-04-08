5b2ba1e2a5a1bbbfee98b8fa9d09f06c10a7fd3b
aethan <aethan@uw.edu>
2017-04-08 12:33:21 -0700

    Add broken javascript

--- webclient/www/script.js
+++ webclient/www/script.js
@@ -1,13 +1,30 @@
-function loadOGP() {
+console.log("test");
+
+function loadOGP(e) {
+    if (e.preventDefault) { e.preventDefault(); }
+
     var title = document.getElementById("title")
     var description = document.getElementById("description")
     var image = document.getElementById("image")
+    var urlForm = document.getElementById("urlForm")
+    console.log("Why isn't this getting called?");
 
-    fetch('http://localhost:4000/v1/summary?url=http://ogp.me/', {
+    fetch('http://localhost:4000/v1/summary?url=' + urlForm.value, {
         method: 'get'
     }).then(function(resp) {
+        //console.log(resp)
+        return resp.json()
+    }).then(function(data) {
+        console.log(data)
+        title.innerHTML = data.title
+        description.innerHTML = data.description
+        image.src = data.image
         
-    })
+    }).catch(function(err) {
+        console.log(err)
+    });
+    // You must return false to prevent the default form behavior
+    return false;
 }
 
-loadOGP()
+//loadOGP()
