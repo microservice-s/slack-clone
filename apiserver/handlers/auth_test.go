ce07c0899bb5fb5a042f76b2617566c88c946fed
aethan <aethan@uw.edu>
2017-04-22 20:59:15 -0700

    Add auth test skeleton

---
+++ apiserver/handlers/auth_test.go
@@ -0,0 +1,19 @@
+package handlers
+
+import "testing"
+
+func TestUsersHandler(t *testing.T) {
+
+}
+
+func TestSessionshandler(t *testing.T) {
+
+}
+
+func TestSessionsMineHandler(t *testing.T) {
+
+}
+
+func TestUsersMeHanlder(t *testing.T) {
+
+}
