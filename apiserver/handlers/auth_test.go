46cc443fc525b3827f57ee931e78de7e5c69fb5e
aethan <aethan@uw.edu>
2017-04-25 21:12:23 -0700

    Clean up

--- apiserver/handlers/auth_test.go
+++ apiserver/handlers/auth_test.go
@@ -656,10 +656,4 @@ func TestUsersMe(t *testing.T) {
 			status, http.StatusOK)
 	}
 
-	// // check the body
-	// if rr.Body.String() != "user signed out\n" {
-	// 	t.Errorf("handler returned unexpected body: \ngot %v \nwant %v",
-	// 		rr.Body.String(), "user signed out\n")
-	// }
-
 }
